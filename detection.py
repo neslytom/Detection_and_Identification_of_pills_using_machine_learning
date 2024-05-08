3e2c21cc678c13e0874c7265457f08ef8e2490df
Nesly Tom <121422333+neslytom@users.noreply.github.com>
2024-05-08 14:08:14 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -14,20 +14,19 @@ def img_prediction(test_image):
     data = []
     img_path = test_image
     testing_img = cv2.imread(img_path)
-    #img_path = "tadalafil 5 MG (18) - Copy.jpg"
-    #testing_img = cv2.imread(img_path)
+    
     cv2.imwrite("..\\PillsDetection\static\\pills_detection.jpg", testing_img)
     model = load_model('mobilenet_model.h5')
     test_image = image.load_img(img_path, target_size=(128, 128))
     test_image = image.img_to_array(test_image)
-    #print("image2",test_image)
+    
     test_image = np.expand_dims(test_image, axis=0)
-    #print("image3",test_image)
+   
     test_image /= 128
     prediction = model.predict(test_image)
-    #print("prediction:",prediction)
+    
     lb = pickle.load(open('label_transform.pkl', 'rb'))
-    #print("result:",lb.inverse_transform(prediction)[0])
+    
     prediction=lb.inverse_transform(prediction)[0]
     print("pred:",prediction)
 
